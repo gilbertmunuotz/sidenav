97048d3465081c0ad297a235418fb3e432c4f38c
gilbertmunuotz <officialgilbert24@gmail.com>
2024-02-20 10:29:20 -0800

    Badoo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import Home from "./pages/Home";
+import About from './pages/About'
+import ContactUs from "./pages/ContactUs";
 import Sidebar from "./components/Sidebar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Sidebar />
+      <BrowserRouter>
+        <Sidebar>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contactus" element={<ContactUs />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Sidebar>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
 function Sidebar() {
+
+    const menuItem = [
+        {
+            path:"/",
+            name:
+        }
+    ]
+
     return (
         <div className='Sidebar'>
 
