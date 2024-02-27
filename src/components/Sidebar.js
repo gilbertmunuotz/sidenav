10b4e6593b4089e3663c14ccf73f9ae00aeff758
gilbertmunuotz <officialgilbert24@gmail.com>
2024-02-27 10:38:49 -0800

    Minor Updates..

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,41 +7,29 @@ const applyClasses = (isOpen, baseClass, conditionalClass) => {
 };
 
 function Sidebar({ children }) {
+    const [isOpen, setIsOpen] = useState(false);
 
-    const [isOpen, setIsOpen] = useState(false)
-
-    const toggle = () => setIsOpen(!isOpen)
+    const toggle = () => {
+        setIsOpen(isOpen ? false : true);
+    };
 
     const menuItems = [
-        {
-            path: "/",
-            name: "Name",
-            icon: <FaTh />
-        },
-        {
-            path: "/contactus",
-            name: "Contact Us",
-            icon: <FaUserAlt />
-        },
-        {
-            path: "/about",
-            name: "About",
-            icon: <FaShoppingBag />
-        },
-    ]
+        { path: "/", name: "Home", icon: <FaTh /> },
+        { path: "/contactus", name: "Contact Us", icon: <FaUserAlt /> },
+        { path: "/about", name: "About", icon: <FaShoppingBag /> },
+    ];
 
     return (
         <div className="container flex">
-            <div className={applyClasses(isOpen, 'sidebar bg-black text-white h-screen w-64 transition-all duration-500 ease-in-out transform', 'w-16')}>
+            <div className={applyClasses(isOpen, 'sidebar bg-black text-white h-screen w-64 transition-all duration-500 ease-in-out transform', 'w-20')}>
                 <div className="top_section flex items-center py-4 px-6">
                     {/* <h1 className={applyClasses(isOpen, 'logo text-2xl', 'hidden')}>Logo</h1> */}
                     <div className="bars ml-auto text-2xl cursor-pointer" onClick={toggle}>
-                        <FaBars onClick={toggle} />
+                        <FaBars />
                     </div>
                 </div>
                 {menuItems.map((item, index) => (
-                    <NavLink key={index} to={item.path}
-                        className={applyClasses(isOpen, 'link flex items-center py-3 px-6 gap-4 hover:bg-[#495057] rounded-lg hover:text-white', 'hover:bg-transparent')}>
+                    <NavLink key={index} to={item.path} className={applyClasses(isOpen, 'link flex items-center py-3 px-8 gap-4 hover:bg-[#495057] rounded-lg hover:text-white', 'hover:bg-transparent')}>
                         <div className="icon text-xl mt-3">{item.icon}</div>
                         <span className={applyClasses(isOpen, 'link_text text-base mt-3', 'hidden')}>{item.name}</span>
                     </NavLink>
@@ -51,7 +39,7 @@ function Sidebar({ children }) {
             <main className="flex-grow px-4 py-4">{children}</main>
 
         </div>
-    )
+    );
 }
 
-export default Sidebar
+export default Sidebar;
